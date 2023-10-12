78b6d7c9d0833b0e824e7c960cdfc859d4005888
ThatAgricBoy <samueljohn3999@gmail.com>
2023-10-12 15:42:49 +0100

    minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Minimum Operations """
+def minOperations(n):
+    """ method that calculates the fewest number of operations needed """
+    if n <= 1:
+        return 0
+    count = 0
+    for i in range(2, n + 1):
+        while n % i == 0:
+            count += i
+            n = n / i
+    return count
