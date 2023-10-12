8d45d6f024260e321550ae9ad608f2278c682a1a
ThatAgricBoy <samueljohn3999@gmail.com>
2023-10-12 16:21:38 +0100

    minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,16 +2,20 @@
 """ Minimum Operations """
 
 
-def minOperations(n: object) -> object:
+def minOperations(n):
     """ method that calculates the fewest number of operations needed
-    @param n:
-    @return:
+        @param n:
+        @return:
     """
     if n <= 1:
         return 0
-    count = 0
+
+    operations = [0] * (n + 1)
+
     for i in range(2, n + 1):
-        while n % i == 0:
-            count += i
-            n = n / i
-    return count
+        operations[i] = float('inf')  # Initialize with a large value
+        for j in range(1, i):
+            if i % j == 0:
+                operations[i] = min(operations[i], operations[j] + i // j)
+
+    return operations[n]
