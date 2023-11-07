93b657e1ce154a01ee380c5eb108ff0d8ef6baa1
ThatAgricBoy <samueljohn3999@gmail.com>
2023-11-07 14:08:54 +0100

    starwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,28 +1,31 @@
 #!/usr/bin/node
+
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/';
+
 const movieId = process.argv[2];
-request(url + movieId, function (error, response, body) {
+const movieEndpoint = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+
+function sendRequest (characterList, index) {
+  if (characterList.length === index) {
+    return;
+  }
+
+  request(characterList[index], (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      sendRequest(characterList, index + 1);
+    }
+  });
+}
+
+request(movieEndpoint, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
-    try {
-      const movieData = JSON.parse(body);
-      if (Array.isArray(movieData.characters)) {
-        movieData.characters.forEach((character) => {
-          request(character, function (error, response, body) {
-            if (error) {
-              console.log(error);
-            } else {
-              console.log(JSON.parse(body).name);
-            }
-          });
-        });
-      } else {
-        console.log("No characters found in this movie.");
-      }
-    } catch (parseError) {
-      console.log("Error parsing JSON response:", parseError);
-    }
+    const characterList = JSON.parse(body).characters;
+
+    sendRequest(characterList, 0);
   }
 });
